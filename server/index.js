9c58a1dff6cb99782fdf251d20a7c00305d09019
ThandokuhleNdlebe <thandotydx@gmail.com>
2024-09-07 18:31:56 +0200

    Created frontend and backend folders

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const 
+const app = express();
+
+//1) MIDDLEWARES
+app.use(express.json());
+
+//2) ROUTE
+
+//3) MONGO DB CONNECTION
+
+//4) GLOBAL ERROR HANDLER
+
+//5) SERVER
